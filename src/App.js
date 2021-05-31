8f3fcb22b701b1bb64dd6e353e2ac6f196003f23
salahxgamer <salahxgamer2015@gmail.com>
2021-05-31 10:55:11 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import AppHeader from './AppHeader.js'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container'
+
+
+function App() {
+  return (
+    <Container maxWidth="xl" disableGutters>
+      <AppHeader />
+      <CssBaseline />
+      mui
+    </Container>
+
+  );
+}
+
+export default App;
